40382e4ccda6560eb2c9e01b8df28f636c4ffa9e
Diego Alves <dieguimac@yahoo.com.br>
2016-10-25 13:21:00 -0200

    Adicionando criar e editar blog fora do painel admin.

--- onetechblog/urls.py
+++ onetechblog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.post_list),
 	url(r'^artigo/(?P<pk>[0-9]+)/$', views.artigo_detalhe),
+	url(r'^artigo/new/$', views.artigo_new, name='artigo_new'),
+	url(r'^artigo/(?P<pk>[0-9]+)/edit/$', views.artigo_edit, name='artigo_edit'),
 
 ]
--- onetechblog/views.py
+++ onetechblog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +12,31 @@ def post_list(request):
 def artigo_detalhe(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/artigo_detalhe.html', {'post':post})
+
+def artigo_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('onetechblog.views.artigo_detalhe', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/artigo_edit.html', {'form': form})
+
+
+def artigo_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.artigo_detalhe', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/artigo_edit.html', {'form': form})
